46f5fb4073f10f40a32e49dd295e49ca23638e10
Samuel Monteiro <smf.monteiro1701@gmail.com>
2024-08-15 00:48:18 -0300

    commit

---
+++ exercicios/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60
+console.log(`a velocidade do seu carro é de ${vel} km/h`)
+if (vel > 60){  //Condição Simples
+    console.log('você ultrapassou o limite de velocidade. MULTADO!')
+}
+console.log('Dirija sempre usando o cinto de segurança')
