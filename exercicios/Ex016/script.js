1b6ad18cbe0950a35ffd1c0e5078029f3d4340ed
Samuel Monteiro <smf.monteiro1701@gmail.com>
2024-08-16 01:15:13 -0300

    commit

--- exercicios/Ex016/script.js
+++ exercicios/Ex016/script.js
@@ -1,1 +1,10 @@
-window.alert('teste...')
+function carregar(){
+    var msg = document.querySelector('div#msg')
+    var img = document.querySelector('div#imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `agora são ${hora} horas`
+
+}
+
+
