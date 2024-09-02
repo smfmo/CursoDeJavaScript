9086088177cd1b0edff005e09faf607627614b63
Samuel Monteiro <smf.monteiro1701@gmail.com>
2024-09-02 07:11:06 -0300

    commit
    Atualização de estudos

---
+++ exercicios/ambienteArray2.js
@@ -0,0 +1,6 @@
+let valores = [5,8,2,9,3,7,8,9,1]
+valores.sort()
+console.log(valores)
+for(var pos = 0; pos < valores.length;pos++ ){
+    console.log(`a posição ${pos} tem o valor ${valores[pos]} `)
+}
---
+++ exercicios/ambienteArray3.js
@@ -0,0 +1,8 @@
+let valor = [2,7,8,4,5,1,9,]
+valor.sort()
+ 
+console.log(valor)
+
+for(pos in valor){
+    console.log(`A posição ${pos} tem o valor ${valor[pos]}`)
+}
