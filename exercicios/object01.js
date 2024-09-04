30dd12aa12e983f64d579691ced50257d9bbcc89
Samuel Monteiro <smf.monteiro1701@gmail.com>
2024-09-04 07:00:26 -0300

    Commit

---
+++ exercicios/object01.js
@@ -0,0 +1,8 @@
+let amigo = {nome: 'josé', 
+    sexo: 'M',
+    peso: 85.4,
+    engordar(p=0){
+        this.peso += p
+    }}
+amigo.engordar(2)
+console.log(`${amigo.nome} pesa ${amigo.peso}kg`)
